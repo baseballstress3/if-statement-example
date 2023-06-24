90257c38d214b3fbec0e313c3301c080c9b4758b
MohammadReza <137616620+baseballstress3@users.noreply.github.com>
2023-06-24 09:00:44 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+flooor = int(input("flooor: "))
+# Adjust flooor
+if flooor > 13 :
+actualFloor = flooor - 1
+else :
+actualFloor = flooor
+print("The elevator will travel to the actual flooor", actualFloor)
